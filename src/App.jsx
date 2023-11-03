5b4932a7ebf6cb38ac4751df8eb8ea41ee7d8fb1
AndreanTan <noobman514@gmail.com>
2023-11-04 00:13:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1 className="font-Inter">Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
