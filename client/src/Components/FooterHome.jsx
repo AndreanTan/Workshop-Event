0dc96fadff6ce8bbce7b42cf168305d07b2c5fe7
evribsap <evribsaputra@gmail.com>
2023-12-08 18:19:51 +0700

    update footer

--- client/src/Components/FooterHome.jsx
+++ client/src/Components/FooterHome.jsx
@@ -2,12 +2,11 @@
 
 function FooterHome() {
     return (
-        <section className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div className="sm:flex sm:items-center sm:justify-between">
-                <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <span className=" text-[#902B1E] self-center text-2xl font-semibold whitespace-nowrap">JELAJAH</span>
-                </a>
-                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
+        <section className="mt-36 max-w-screen-xl mx-auto p-4 md:py-8">
+            <hr class=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <div className="md:flex md:items-center md:justify-between">
+            <span className="block text-sm text-gray-500 sm:text-center ">&copy; {new Date().getFullYear()} <a href="/" className="hover:underline">JELAJAH</a>. All Rights Reserved.</span>
+                <u className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">Email</a>
                     </li>
@@ -20,10 +19,8 @@ function FooterHome() {
                     <li>
                         <a href="#" className="hover:underline">Travel</a>
                     </li>
-                </ul>
+                </u>
             </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center ">&copy; {new Date().getFullYear()} <a href="/" className="hover:underline">JELAJAH</a>. All Rights Reserved.</span>
         </section>
     )
 }
