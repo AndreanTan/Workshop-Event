887c69983057e429c08b3a284686343c4ccde73a
evribsap <evribsaputra@gmail.com>
2023-12-09 13:16:56 +0700

    update footer

--- client/src/Components/FooterHome.jsx
+++ client/src/Components/FooterHome.jsx
@@ -6,7 +6,7 @@ function FooterHome() {
             <hr class=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div className="md:flex md:items-center md:justify-between">
             <span className="block text-sm text-gray-500 sm:text-center ">&copy; {new Date().getFullYear()} <a href="/" className="hover:underline">JELAJAH</a>. All Rights Reserved.</span>
-                <u className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">Email</a>
                     </li>
@@ -19,7 +19,7 @@ function FooterHome() {
                     <li>
                         <a href="#" className="hover:underline">Travel</a>
                     </li>
-                </u>
+                </ul>
             </div>
         </section>
     )
