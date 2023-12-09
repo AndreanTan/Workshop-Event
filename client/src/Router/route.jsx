1325877517ceaa4b2e1feaa441c7d83a997bb046
evribsap <evribsaputra@gmail.com>
2023-12-09 15:10:34 +0700

    add page descworkshop

--- client/src/Router/route.jsx
+++ client/src/Router/route.jsx
@@ -5,6 +5,8 @@ import { CartComponent } from "../Pages/Cart";
 import HomeComponent from "../Pages/Home";
 import Workshop from "../Pages/Workshop";
 import WorkshopCard from "../Components/WorkshopCard";
+import DescriptionWorkshop from "../Pages/DescriptionWorkshop";
+
 
 
 
@@ -13,6 +15,7 @@ const route = [
   <Route key={"/workshop"} path="/workshop" element={<Workshop />} />,
   <Route key={"/cart"} path="/cart" element={<CartComponent />} />,
   <Route key={"/card"} path="/card" element={<WorkshopCard />} />,
+  <Route key={"/descworkshop"} path="/descworkshop" element={<DescriptionWorkshop />} />,
   <Route key={"/login"} path="/login" element={<Auth isLogin={false} />} />,
 ];
 
